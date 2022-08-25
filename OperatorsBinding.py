1213cc9fc1aeaa8789fafafdeb9cf5d9abd71363
Bosun Olisa <OLATUNBOSUN.OLISA93@gmail.com>
2022-08-25 05:06:58 +0000

    created sample input function

--- OperatorsBinding.py
+++ OperatorsBinding.py
@@ -1,40 +1,6 @@
-a = 0b1001
-b = 0b1100
-bin(a | b)
-'0b1101'
-bin(a & b)
-'0b1000'
-bin(a ^ b)
-'0b101'
-a = 0b110
-bin(a >> 2)
-'0b1'
-bin(a >> 4)
-'0b0'
-a
-6
-a = 0b110
-bin(a << 2)
-'0b11000'
-a << 2
-24
-not True
-False
-not False
-True
-True or True
-True
-True or False
-True
-False or False
-False
-False or True
-True
-True and True
-True
-True and False
-False
-False and False
-False
-False and True
-False
+a = 0b010
+b = 0b011
+
+print(a + b) 
+
+
