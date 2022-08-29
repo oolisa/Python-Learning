1b50c81127938b781f0144e5fcf66e2a1305557e
Bosun Olisa <OLATUNBOSUN.OLISA93@gmail.com>
2022-08-29 19:55:30 +0000

    final changes to remove and list services in list

--- AWS-Services-List.py
+++ AWS-Services-List.py
@@ -5,7 +5,6 @@ awsservices_list = []
 
 #2. Populate the list using append or insert.
 
-awsservices_list.append("S3")
 awsservices_list.append("EC2")
 awsservices_list.append("IAM")
 awsservices_list.append("S3")
@@ -14,13 +13,20 @@ awsservices_list.append("CloudFormation")
 awsservices_list.append("Elastic Beanstalk")
 
 #3. Print the list and length of the list.
-print(awsservices_list)
-print (len(awsservices_list))
+#print(awsservices_list)
+#print (len(awsservices_list))
 
 #4. Remove two specific services from the list by name or by index.
 
 awsservices_list.remove("VPC")
 awsservices_list.remove("CloudFormation")
+print(awsservices_list)
+
+#5. Print the new list and the new length of the list.
+
+print(awsservices_list)
+print (len(awsservices_list))
+
 
 
 
