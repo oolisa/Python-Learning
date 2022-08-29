12af390bf5efa1496e7a15da9bd5ebf64111c000
Bosun Olisa <OLATUNBOSUN.OLISA93@gmail.com>
2022-08-29 18:32:33 +0000

    Created a list of AWS Services

---
+++ AWS-Services-List.py
@@ -0,0 +1,30 @@
+# AWS List of Services
+
+#1. Create an empty list. 
+awsservices_list = []
+
+#2. Populate the list using append or insert.
+
+awsservices_list.append("S3")
+awsservices_list.append("EC2")
+awsservices_list.append("IAM")
+awsservices_list.append("S3")
+awsservices_list.append("VPC")
+awsservices_list.append("CloudFormation")
+awsservices_list.append("Elastic Beanstalk")
+
+#3. Print the list and length of the list.
+print(awsservices_list)
+print (len(awsservices_list))
+
+#4. Remove two specific services from the list by name or by index.
+
+awsservices_list.remove("VPC")
+awsservices_list.remove("CloudFormation")
+
+
+
+
+
+
+
