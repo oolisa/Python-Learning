226c5ca890552e2bf2967e4123058bfcb8610c43
Bosun Olisa <OLATUNBOSUN.OLISA93@gmail.com>
2022-09-01 18:51:46 +0000

    Added is_palindrome function and build_list function

--- Tutorials/testing-functions.py
+++ Tutorials/testing-functions.py
@@ -1,9 +1,8 @@
 # 1) Write a `split_name` function that takes a string and returns a dictionary with first_name and last_name
 
 def split_name(name):
-    names = name.split()
-    first_name = names[0]
-    last_name = names[-1]
+    first_name, last_name = name.split(maxsplit=1)
+
     return { 
         'first_name': first_name,
         'last_name': last_name,
@@ -23,6 +22,10 @@ assert split_name("Victor Von Doom") == {
 }, f"Expected {{'first_name': 'Victor', 'last_name': 'Von Doom'}} but received {split_name('Victor Von Doom')}"
 
 # 3) Write an `is_palindrome` function to check if a string is a palindrome (reads the same from left-to-right and right-to-left)
+def is_palindrome(item):
+    item = str(item)
+    return item == item[::-1]
+    
 assert is_palindrome("radar") == True, f"Expected True but got {is_palindrome('radar')}"
 assert is_palindrome("stop") == False, f"Expected False but got {is_palindrome('stop')}"
 
@@ -33,6 +36,12 @@ assert is_palindrome(10) == False, f"Expected False but got {is_palindrome(10)}"
 
 # 5) Write a `build_list` function that takes an item and a number to include in a list
 
+def build_list(item, count=1):
+    items = []
+    for _ in range(count):
+        items.append(item)
+    return items
+    
 assert build_list("Apple", 3) == [
     "Apple",
     "Apple",
