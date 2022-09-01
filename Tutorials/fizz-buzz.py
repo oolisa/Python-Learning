864fbd7053a88d9486ad7203e2c016641001557c
Bosun Olisa <OLATUNBOSUN.OLISA93@gmail.com>
2022-09-01 03:45:58 +0000

    Updated Fizz Buzz script to meet all conditions

---
+++ Tutorials/fizz-buzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.7
+
+upper_number = int(input("How many values should we process:"))
+
+for number in range(1, upper_number + 1):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    else:
+        print(number)
+    
