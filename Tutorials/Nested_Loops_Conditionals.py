d81a7147a681d1e128cc316d47e20d10109d4331
Bosun Olisa <OLATUNBOSUN.OLISA93@gmail.com>
2022-09-01 03:02:56 +0000

    Added use of continue and break statements

--- Tutorials/Nested_Loops_Conditionals.py
+++ Tutorials/Nested_Loops_Conditionals.py
@@ -1,1 +1,31 @@
+#counter = 1
+#while counter <= 25: #while counter less than equal to 25.
+#    if counter % 4 == 0: #if counter is divisible by 4
+#        print(counter)
+#    counter += 1  #increment the counter.
+    
+#continue and break statements
 
+#count = 0
+#while count < 10:
+#    if count % 2 == 0:
+#        count += 1
+#        continue
+#    print(f"We're counting odd numbers: {count}")
+#    count += 1
+    
+#count = 1
+#while count < 10:
+#    if count % 2 == 0:
+#        break
+#    print(f"We're counting odd numbers: {count}")
+#    count += 1
+    
+
+colors = ['blue', 'green', 'red', 'purple']
+for color in colors:
+    if color == 'blue':
+        continue
+    elif color == 'red':
+        break
+    print(color)
