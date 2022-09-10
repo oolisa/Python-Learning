1c61df10f19058e29b580c8400dc9ad9ab44990d
Bosun Olisa <olatunbosun.olisa93@gmail.com>
2022-09-10 12:57:59 -0500

    Final Changes

--- Tutorials/fib.py
+++ Tutorials/fib.py
@@ -4,9 +4,10 @@ def fib(n):
     elif n == 1:
         return 1
         
-    return fib(n-2) + fib(n-1)
+    return fib(n - 2) + fib(n - 1)
     
-item_to_calculate = int(input("What Fibonacci item would you like to calculate? ")
-print(fib(item_to_calculate)) 
+item_to_calculate = int(input("What Fibonacci item would you like to calculate? "))
+print(fib(item_to_calculate))
+
 
 
