60185267a0bca55744540d2937086d72db2f627c
Bosun Olisa <OLATUNBOSUN.OLISA93@gmail.com>
2022-09-11 02:56:35 +0000

    Create a Table named Music

--- Scripts/s3/upload_object.py
+++ Scripts/s3/upload_object.py
@@ -1,10 +1,10 @@
 import boto3
+
 s3 = boto3.client('s3')
 
 filename="search_s3_bucket.py"
 
 with open(filename, 'rb') as data:
     s3.upload_fileobj(data, 'otdynamic3', filename)
-    
     print("Object has been uploaded") 
- 
+  
