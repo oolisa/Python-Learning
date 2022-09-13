315023ce83694cfd5337473a9af391d3827fd0c5
Bosun Olisa <OLATUNBOSUN.OLISA93@gmail.com>
2022-09-13 21:57:59 +0000

    Created Vpc and added response

---
+++ Scripts/vpc/create_vpc.py
@@ -0,0 +1,13 @@
+#How to Create VPC using Python Boto3
+
+import boto3
+
+client=boto3.client("ec2")
+
+client.create_vpc(CidrBlock='10.1.0.0/16')
+
+response = client.create_vpc(
+    CidrBlock='10.1.0.0/16',
+)
+
+print(response)
