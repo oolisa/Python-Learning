ff289f75dfefb089707f705b44b1f9b5bc5e15a9
Bosun Olisa <OLATUNBOSUN.OLISA93@gmail.com>
2022-09-13 22:05:51 +0000

    Added 2nd method of creating VPC without response

--- Scripts/vpc/create_vpc.py
+++ Scripts/vpc/create_vpc.py
@@ -1,5 +1,7 @@
 #How to Create VPC using Python Boto3
 
+#Method 1 via documentation : 
+
 import boto3
 
 client=boto3.client("ec2")
@@ -9,3 +11,7 @@ response = client.create_vpc(
 )
 
 print(response)
+
+#Method 2 without response : 
+
+client.create_vpc(CidrBlock='10.1.0.0/16')
